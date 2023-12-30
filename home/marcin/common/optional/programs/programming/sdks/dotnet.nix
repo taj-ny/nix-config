4251a9a33ba066541212abe6092d5541d15eff65
taj-ny <79316397+taj-ny@users.noreply.github.com>
2023-12-30 21:49:16 +0100

    home/marcin/dotnet: remove broken workload fix

--- home/marcin/common/optional/programs/programming/sdks/dotnet.nix
+++ home/marcin/common/optional/programs/programming/sdks/dotnet.nix
@@ -4,21 +4,7 @@ let
   dotnet-combined = with pkgs; (dotnetCorePackages.combinePackages [
     dotnet-sdk_7
     dotnet-sdk_8
-  ]).overrideAttrs (finalAttrs: previousAttrs: {
-    # This is needed to install workload in $HOME
-    # https://discourse.nixos.org/t/dotnet-maui-workload/20370/2
-    # (doesn't actually work)
-
-    postBuild = (previousAttrs.postBuild or '''') + ''
-
-      for i in $out/sdk/*
-      do
-        i=$(basename $i)
-        mkdir -p $out/metadata/workloads/''${i/-*}
-        touch $out/metadata/workloads/''${i/-*}/userlocal
-      done
-    '';
-  });
+  ]);
 in
 {
   home.sessionVariables.DOTNET_ROOT = "${dotnet-combined}";
