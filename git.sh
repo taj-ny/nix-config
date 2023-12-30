3de860fca27035af029201145b39125af44ab944
taj-ny <79316397+taj-ny@users.noreply.github.com>
2023-12-30 18:36:32 +0100

    git.sh: update description

--- git.sh
+++ git.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-# This script makes it easier to manage the git repository in a root-owned directory with 755 permissions.
+# A wrapper script for git that copies my ssh keys to root's directory in order to make it easier to manage the repository in a root-owned directory with 755 permissions.
 
 if (( $EUID != 0 )); then
     echo "Please run as root"
     exit
 fi
 
-HOME=/home/marcin
-
 mkdir -p /root/.ssh
-cp $HOME/.ssh/* /root/.ssh
+cp /home/marcin/.ssh/* /root/.ssh
 git "$@"
 rm -r /root/.ssh
