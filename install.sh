74658e077f4d685b5774dc745d594e6b512d788b
taj-ny <79316397+taj-ny@users.noreply.github.com>
2023-12-31 18:50:55 +0100

    install.sh: create

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+sudo nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko "$DISK_LAYOUT" --arg disks '[ "$DISK" ]'
+
+cd ..
+sudo chown -R root:root nix-config
+sudo chmod -R 755 nix-config
+sudo mv nix-config /mnt/nix/config
+cd /mnt/nix/config
+
+sudo btrfs filesystem mkswapfile --size "$SWAPSIZE" /mnt/swap/swapfile
+sudo swapon /mnt/swap/swapfile
+
+sudo mkdir /mnt/persist/passwords
+sudo chmod 700 /mnt/persist/passwords
+mkpasswd -m sha-512 | sudo tee /mnt/persist/passwords/marcin
+
+sudo nixos-install --flake ".#$CONFIGURATION"
+sudo reboot
