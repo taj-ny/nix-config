7b7ad5bd60820e789865c4b9d6a3e5d73c893064
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-01-07 18:28:10 +0100

    home/marcin/konsole: rename Main.profile to konsole.profile

--- home/marcin/common/optional/programs/terminal/konsole/default.nix
+++ home/marcin/common/optional/programs/terminal/konsole/default.nix
@@ -1,9 +1,9 @@
 {
-  desktop.kde.configs.konsolerc."Desktop Entry".DefaultProfile = "Main.profile";
+  desktop.kde.configs.konsolerc."Desktop Entry".DefaultProfile = "konsole.profile";
 
   home.file = {
     # Breeze with background colors from Krita Dark
     ".local/share/konsole/konsole.colorscheme".source = ./konsole.colorscheme;
-    ".local/share/konsole/Main.profile".source = ./Main.profile;
+    ".local/share/konsole/konsole.profile".source = ./konsole.profile;
   };
 }
