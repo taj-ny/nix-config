7d94384e5a5c0114af6736bb381ea77d2d3c2766
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-01-07 17:23:37 +0100

    nixos: silent boot

--- nixos/common/global/boot.nix
+++ nixos/common/global/boot.nix
@@ -1,11 +1,18 @@
 {
-  boot.loader = {
-    systemd-boot = {
-      enable = true;
-      editor = false;
-      configurationLimit = 10;
+  boot = {
+    consoleLogLevel = 0;
+    kernelParams = [ "quiet" "udev.log_level=3" ];
+
+    loader = {
+      systemd-boot = {
+        enable = true;
+        editor = false;
+        configurationLimit = 10;
+      };
+
+      efi.canTouchEfiVariables = true;
     };
 
-    efi.canTouchEfiVariables = true;
+    initrd.verbose = false;
   };
 }
