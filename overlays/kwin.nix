1efd61309b907e7de885ca6a8793495053cac4c4
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-01-13 15:35:35 +0100

    overlays/kwin: add hash

--- overlays/kwin.nix
+++ overlays/kwin.nix
@@ -5,6 +5,6 @@ prev.plasma5Packages.plasma5.kwin.overrideAttrs (old: {
     owner = "taj-ny";
     repo = "kwin-forceblur-wayland";
     rev = "b2073237dd32b4545d1522a530359922ad7d35a6";
-    hash = "";
+    hash = "sha256-l0a5hQPRQagQ89QE3qqNYAWtIPZThFksyMiIodPhUIQ=";
   };
 })
