2898c146323ec10be68b8852922af99f288f569b
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-01-13 15:20:09 +0100

    overlays/kwin: change src to taj-ny/kwin-forceblur-wayland

---
+++ overlays/kwin.nix
@@ -0,0 +1,10 @@
+{ prev }:
+
+prev.plasma5Packages.plasma5.kwin.overrideAttrs (old: {
+  src = prev.fetchFromGitHub {
+    owner = "taj-ny";
+    repo = "kwin-forceblur-wayland";
+    rev = "b2073237dd32b4545d1522a530359922ad7d35a6";
+    hash = "";
+  };
+})
