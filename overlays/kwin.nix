7ea36b231a8ec546681225903b7e18fa1264af29
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-02-27 16:05:17 +0100

    overlays/kwin: use tag instead of commit hash

--- overlays/kwin.nix
+++ overlays/kwin.nix
@@ -4,7 +4,7 @@ prev.plasma5Packages.plasma5.kwin.overrideAttrs (old: {
   src = prev.fetchFromGitHub {
     owner = "taj-ny";
     repo = "kwin-forceblur-wayland";
-    rev = "b2073237dd32b4545d1522a530359922ad7d35a6";
+    rev = "v5.27.10";
     hash = "sha256-l0a5hQPRQagQ89QE3qqNYAWtIPZThFksyMiIodPhUIQ=";
   };
 })
