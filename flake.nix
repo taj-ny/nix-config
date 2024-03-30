229079e31a91dd3aabfdb824e7c1052b64eeea01
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-03-30 09:48:28 +0100

    flake: add nixpkgs-staging-next to inputs

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-staging-next.url = "github:nixos/nixpkgs/staging-next";
 
     hardware.url = "github:nixos/nixos-hardware";
     impermanence.url = "github:nix-community/impermanence";
