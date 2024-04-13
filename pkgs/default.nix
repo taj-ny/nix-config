1ff442061c507b11f40b58e84be32f764fe73903
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-04-13 16:43:02 +0200

    pkgs/paneltransparencybutton: remove

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,7 +5,6 @@ rec {
   klassy = pkgs.kdePackages.callPackage ./klassy.nix { };
   lightlyshaders = pkgs.kdePackages.callPackage ./lightlyshaders.nix { };
   nix-ascii-art = pkgs.callPackage ./nix-ascii-art.nix { inherit rascii; };
-  paneltransparencybutton = pkgs.callPackage ./paneltransparencybutton.nix { };
   plasma-panel-colorizer = pkgs.callPackage ./plasma-panel-colorizer.nix { };
   plasma-panel-spacer-extended = pkgs.callPackage ./plasma-panel-spacer-extended.nix { };
   plasma-window-title-applet = pkgs.callPackage ./plasma-window-title-applet.nix { };
