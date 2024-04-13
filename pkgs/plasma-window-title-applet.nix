a0e3cda040acdccb6f320132c8aa01395f24a753
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-04-13 16:09:27 +0200

    pkgs/plasma-window-title-applet: 0.5 -> 0.5.5

--- pkgs/plasma-window-title-applet.nix
+++ pkgs/plasma-window-title-applet.nix
@@ -5,22 +5,22 @@
 
 stdenv.mkDerivation rec {
   pname = "plasma-window-title-applet";
-  version = "0.5";
+  version = "0.5.5";
 
   src = fetchFromGitHub {
     owner = "dhruv8sh";
     repo = "plasma6-window-title-applet";
-    rev = "v${version}";
-    hash = "sha256-B0MlnGbhBFSsX9Bc4uQvdvgMEifRGFOOLkgMAoCzWhE=";
+    rev = "6d6b939bb8138a8b1640cf2f6d395a3030d7bbaa";
+    hash = "sha256-dfJcRbUubv3/1PAWCFtNWzc8nyIcgTW39vryFLOOqzs=";
   };
 
   installPhase = ''
     mkdir -p $out/share/plasma/plasmoids/org.kde.windowtitle
-    cp -r $src/package/* $out/share/plasma/plasmoids/org.kde.windowtitle
+    cp -r $src/metadata.json $src/contents $out/share/plasma/plasmoids/org.kde.windowtitle
   '';
 
   meta = {
-    description = "Plasma 6 Window Title applet ";
+    description = "Plasma 6 Window Title applet";
     homepage = "https://github.com/dhruv8sh/plasma6-window-title-applet";
   };
 }
