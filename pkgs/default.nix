483ca73c830a7b1895c3874d945d0da0c5a71cd8
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-04-23 09:08:58 +0200

    Revert "pkgs/plasma-desktop-indicator: init at 0.3"
    This reverts commit e3e23f74f3647cc5d04e150a0f55bc5ea186fd02.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,7 +7,6 @@ rec {
   lightlyshaders = pkgs.kdePackages.callPackage ./lightlyshaders.nix { };
   nix-ascii-art = pkgs.callPackage ./nix-ascii-art.nix { inherit rascii; };
   plasma-breath-wallpapers = pkgs.callPackage ./plasma-breath-wallpapers.nix { };
-  plasma-desktop-indicator = pkgs.callPackage ./plasma-desktop-indicator.nix { };
   plasma-panel-colorizer = pkgs.callPackage ./plasma-panel-colorizer.nix { };
   plasma-panel-spacer-extended = pkgs.callPackage ./plasma-panel-spacer-extended.nix { };
   plasma-window-title-applet = pkgs.callPackage ./plasma-window-title-applet.nix { };
