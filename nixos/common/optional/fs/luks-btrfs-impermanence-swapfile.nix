0d2ab59796c9cbd928ec05e2101f034f92518ed2
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-04-26 16:07:26 +0200

    nixos/fs: add luks-btrfs-impermanence layout

--- nixos/common/optional/fs/luks-btrfs-impermanence-swapfile.nix
+++ nixos/common/optional/fs/luks-btrfs-impermanence-swapfile.nix
@@ -2,44 +2,18 @@ let
   btrfsOptions = [ "noatime" "nodiratime" "discard=async" ];
 in
 {
-  boot = {
-    # Add kernelParams = [ "resume_offset=..." ]; to device-specific configuration
-    # btrfs inspect-internal map-swapfile -r /swap/swapfile
-    resumeDevice = "/dev/mapper/nixos";
+  imports = [
+    ./luks-btrfs-impermanence.nix
+  ];
 
-    initrd.luks.devices."nixos".device = "/dev/disk/by-partlabel/nixos-luks";
-  };
+  # Add kernelParams = [ "resume_offset=..." ]; to device-specific configuration
+  # btrfs inspect-internal map-swapfile -r /swap/swapfile
+  boot.resumeDevice = "/dev/mapper/nixos";
 
-  fileSystems = {
-    "/boot" = {
-      device = "/dev/disk/by-partlabel/boot";
-      fsType = "vfat";
-    };
-
-    "/home" = {
-      device = "/dev/mapper/nixos";
-      fsType = "btrfs";
-      options = [ "subvol=/@home" ] ++ btrfsOptions;
-    };
-
-    "/nix" = {
-      device = "/dev/mapper/nixos";
-      fsType = "btrfs";
-      options = [ "subvol=/@nix" ] ++ btrfsOptions;
-    };
-
-    "/persist" = {
-      device = "/dev/mapper/nixos";
-      fsType = "btrfs";
-      neededForBoot = true;
-      options = [ "subvol=/@persist" ] ++ btrfsOptions;
-    };
-
-    "/swap" = {
-      device = "/dev/mapper/nixos";
-      fsType = "btrfs";
-      options = [ "subvol=/@swap" ] ++ btrfsOptions;
-    };
+  fileSystems."/swap" = {
+    device = "/dev/mapper/nixos";
+    fsType = "btrfs";
+    options = [ "subvol=/@swap" ] ++ btrfsOptions;
   };
 
   swapDevices = [
