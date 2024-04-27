43b2e8703cafd76e34039e4479330e360d823d86
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-04-27 12:42:53 +0200

    home/marcin/konsole: disable toolbars

--- home/marcin/common/optional/programs/terminal/konsole/default.nix
+++ home/marcin/common/optional/programs/terminal/konsole/default.nix
@@ -5,5 +5,10 @@
     # Breeze with background colors from Krita Dark
     ".local/share/konsole/konsole.colorscheme".source = ./konsole.colorscheme;
     ".local/share/konsole/konsole.profile".source = ./konsole.profile;
+
+    # Someone had a brilliant idea that the toolbar configuration should be stored in an encoded string. I don't even
+    # know what's encoded in there, I just launched Konsole in a sandbox, disabled toolbars, closed it and copied the
+    # file. I love KDE.
+    ".local/share/konsole/konsolestaterc".source = ./konsolestaterc;
   };
 }
