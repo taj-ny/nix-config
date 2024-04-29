9ee8ced9e04ffa2e6835bc73931b1477956e2aa6
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-04-29 14:37:36 +0200

    nixos/u2f-pam: disable for kde

--- nixos/common/optional/security/u2f-pam.nix
+++ nixos/common/optional/security/u2f-pam.nix
@@ -4,17 +4,15 @@
   environment.systemPackages = with pkgs; [ pam_u2f ];
   services.udev.packages = with pkgs; [ pam_u2f ];
 
-  security.pam.u2f = {
-    enable = true;
-    cue = true;
-    interactive = true;
-    control = "required";
-    authFile = "/persist/u2f_keys";
-  };
+  security.pam = {
+    u2f = {
+      enable = true;
+      cue = true;
+      interactive = true;
+      control = "required";
+      authFile = "/persist/u2f_keys";
+    };
 
-  security.pam.services = {
-    login.u2fAuth = true;
-    kde.u2fAuth = true;
-    sudo.u2fAuth = true;
+    services.kde.u2fAuth = false;
   };
 }
