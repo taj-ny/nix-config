dc6157b88f58a4bef73f7459b1100513dd8470b6
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-05-03 06:06:47 +0200

    nixos: fix remote deployment

--- nixos/common/optional/allow-remote-deployment.nix
+++ nixos/common/optional/allow-remote-deployment.nix
@@ -1,9 +1,13 @@
+{ pkgs, ... }:
+
 {
   users = {
     groups.nixdeploy = {};
     users.nixdeploy = {
       isSystemUser = true;
       group = "nixdeploy";
+      extraGroups = [ "wheel" ];
+      shell = pkgs.bash;
 
       openssh.authorizedKeys.keys = [
         ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIrMW7NUE7i3/6AksQmq34RK1xL2krFA0ljv8AiPho9Z marcin@andromeda''
@@ -12,4 +16,16 @@
   };
 
   nix.settings.trusted-users = [ "nixdeploy" ];
+
+  security.sudo.extraRules = [
+    {
+      users = [ "nixdeploy" ];
+      commands = [
+        {
+          command = "ALL";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
 }
