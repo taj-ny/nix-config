e5922237fd7a8a6b55c0c416a1d9a47ab115933c
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-06-09 08:17:20 +0200

    home/marcin/easyeffects: use services.easyeffects option

--- home/marcin/common/optional/programs/audio/easyeffects/default.nix
+++ home/marcin/common/optional/programs/audio/easyeffects/default.nix
@@ -5,18 +5,5 @@
     ./_config.nix
   ];
 
-  home = {
-    packages = with pkgs; [ easyeffects ];
-    
-    file.".config/autostart/easyeffects-service.desktop".text = ''
-      [Desktop Entry]
-      Name=Easy Effects
-      Comment=Easy Effects Service
-      Exec=easyeffects --gapplication-service
-      Icon=com.github.wwmm.easyeffects
-      StartupNotify=false
-      Terminal=false
-      Type=Application
-    '';
-  };
+  services.easyeffects.enable = true;
 }
