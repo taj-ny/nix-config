7ca73747a17aa44b224200aa1cd2bd1b1920cb14
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-07-14 13:45:50 +0200

    home/marcin/konsole: don't ask for confirmation on close

--- home/marcin/common/optional/programs/terminal/konsole/default.nix
+++ home/marcin/common/optional/programs/terminal/konsole/default.nix
@@ -1,14 +1,15 @@
 {
-  programs.plasma.configFile.konsolerc."Desktop Entry".DefaultProfile.value = "konsole.profile";
+  programs.plasma.configFile.konsolerc = {
+    "Desktop Entry".DefaultProfile.value = "konsole.profile";
+    "Notification Messages".CloseAllTabs.value = true;
+  };
 
   home.file = {
     # Breeze with background colors from Krita Dark
     ".local/share/konsole/konsole.colorscheme".source = ./konsole.colorscheme;
     ".local/share/konsole/konsole.profile".source = ./konsole.profile;
 
-    # Someone had a brilliant idea that the toolbar configuration should be stored in an encoded string. I don't even
-    # know what's encoded in there, I just launched Konsole in a sandbox, disabled toolbars, closed it and copied the
-    # file. I love KDE.
+    # Why the hell is the toolbar configuration stored in an encoded string?
     ".local/share/konsole/konsolestaterc".source = ./konsolestaterc;
   };
 }
