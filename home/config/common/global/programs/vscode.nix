0b154e1f543022b15ba132ec1f631e6ac71b87d6
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-08-06 14:42:27 +0200

    .

--- home/config/common/global/programs/vscode.nix
+++ home/config/common/global/programs/vscode.nix
@@ -12,9 +12,9 @@
     userSettings = {
       "material-icon-theme.folders.theme" = "classic";
       "material-icon-theme.folders.color" = "#009688";
-
       "nix.enableLanguageServer" = true;
       "nix.serverPath" = "${pkgs.nixd}/bin/nixd";
+      "workbench.iconTheme" = "material-icon-theme";
     };
   };
 }
--- scripts/fix-permissions.sh
+++ scripts/fix-permissions.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd /nix/config
-chown nixconfig:nixconfig -R .
-chown root:root -R .git *.sh **/*.sh scripts
+chown marcin:users -R .
+chown root:root -R .git *.sh .vscode/tasks.json **/*.sh scripts
