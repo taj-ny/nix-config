17a3e7357861d326deecc086e0f94c4b209e92a5
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-08-06 17:53:59 +0200

    shell/gcc: remove

--- shell.nix
+++ shell.nix
@@ -8,10 +8,4 @@
     NIX_CONFIG = "experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [ nix home-manager git ];
   };
-
-  gcc = pkgs.mkShell rec {
-    buildInputs = with pkgs; [
-      gcc
-    ];
-  };
 }
