bc235a883892f2d5d2fd3b24c711e8742f628629
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-08-06 14:53:26 +0200

    home/config/*/vscode: change terminal font to match konsole

--- home/config/common/global/programs/vscode.nix
+++ home/config/common/global/programs/vscode.nix
@@ -14,6 +14,9 @@
       "material-icon-theme.folders.color" = "#009688";
       "nix.enableLanguageServer" = true;
       "nix.serverPath" = "${pkgs.nixd}/bin/nixd";
+      "terminal.integrated.fontFamily" = "MesloLGS NF";
+      "terminal.integrated.fontSize" = 13;
+      "terminal.integrated.lineHeight" = 1;
       "workbench.iconTheme" = "material-icon-theme";
     };
   };
