2726f72de8de6e6d8dce1589674224918225f858
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-08-06 14:43:08 +0200

    pkgs/vscode-nvim: remove

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,12 +7,10 @@ rec {
   nix-ascii-art = pkgs.callPackage ./nix-ascii-art.nix { inherit rascii; };
   nixos-update-notifier = pkgs.callPackage ./nixos-update-notifier { };
   plasma-breath-wallpapers = pkgs.callPackage ./plasma-breath-wallpapers.nix { };
-  #plasma-panel-colorizer = pkgs.kdePackages.callPackage ./plasma-panel-colorizer.nix { };
   plasma-window-title-applet = pkgs.callPackage ./plasma-window-title-applet.nix { };
   polonium = pkgs.callPackage ./polonium.nix { };
   rascii = pkgs.callPackage ./rascii.nix { };
   usbkill = pkgs.python3Packages.callPackage ./usbkill.nix { };
-  vscode-nvim = pkgs.callPackage ./vscode-nvim.nix { buildVimPlugin = pkgs.vimUtils.buildVimPlugin; };
 
   patchDesktop = import ./patch-desktop.nix { inherit pkgs; };
 }
