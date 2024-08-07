01c2281174994e13d4527246483a13837bf08ed4
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-08-07 21:11:00 +0200

    home/config/*/btop: sort processes by cpu direct

--- home/config/common/programs/btop.nix
+++ home/config/common/programs/btop.nix
@@ -1,6 +1,7 @@
 {
   programs.btop.settings = {
     disks_filter = builtins.concatStringsSep " " [ "/" "/boot" "/nix" ];
+    proc_sorting = "cpu direct";
     swap_disk = false;
     theme_background = false;
     update_ms = 1000;
