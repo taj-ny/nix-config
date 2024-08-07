aa0590a5b3c98d489e30f0ad536ed9481fac2b4e
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-08-07 21:07:03 +0200

    home/config/*/btop: disable background

--- home/config/common/programs/btop.nix
+++ home/config/common/programs/btop.nix
@@ -2,6 +2,7 @@
   programs.btop.settings = {
     disks_filter = builtins.concatStringsSep " " [ "/" "/boot" "/nix" ];
     swap_disk = false;
+    theme_background = false;
     update_ms = 1000;
   };
 }
