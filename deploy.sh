eda8cfd5a6b509b870c55d2d4c27df9f51878d49
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-08-08 13:56:04 +0200

    deploy.sh: diff with /tmp/nix-config, not /nix/config

--- deploy.sh
+++ deploy.sh
@@ -11,16 +11,15 @@ rm -f ~/.gtkrc-2.0
 sudo rm -rf /tmp/nix-config
 sudo cp -r /nix/config /tmp/nix-config
 sudo chown root:root -R /tmp/nix-config
-cd /tmp/nix-config
 
 if test -f /etc/current-config; then
     EXIT_CODE=0
-    diff -r --exclude=.git $(cat /etc/current-config) /nix/config > /dev/null 2>&1 || EXIT_CODE=$?
+    diff -r --exclude=.git $(cat /etc/current-config) /tmp/nix-config > /dev/null 2>&1 || EXIT_CODE=$?
     if [ $EXIT_CODE = 0 ]; then
         echo "No changes."
         exit 1
     fi
-    diff -Naur --color=always --exclude=.git $(cat /etc/current-config) /nix/config | less -R
+    diff -Naur --color=always --exclude=.git $(cat /etc/current-config) /tmp/nix-config | less -R
     read -n 1 -p "Deploy? (y/n)" confirm
     echo $confirm > /home/marcin/test
     if [ "$confirm" = "${confirm#[Yy]}" ]; then
@@ -30,4 +29,4 @@ else
     echo -e "\033[1;33mwarning:\033[0m /etc/current-config not found. Cannot show diff."
 fi
 
-sudo nixos-rebuild switch --flake .#$HOST --log-format internal-json -v |& nom --json
+sudo nixos-rebuild switch --flake /tmp/nix-config#$HOST --log-format internal-json -v |& nom --json
