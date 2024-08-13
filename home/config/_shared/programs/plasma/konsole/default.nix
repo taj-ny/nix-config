2d332ac10afbfc8188e7d6602b0048c6607db09c
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-08-13 19:44:45 +0200

    home/config/*/plasma/konsole: fix toolbars

--- home/config/_shared/programs/plasma/konsole/default.nix
+++ home/config/_shared/programs/plasma/konsole/default.nix
@@ -3,11 +3,11 @@
     # Breeze with background colors from Krita Dark
     ".local/share/konsole/konsole.colorscheme".source = ./konsole.colorscheme;
     ".local/share/konsole/konsole.profile".source = ./konsole.profile;
-    # Why the hell is the toolbar configuration stored in an encoded string?
-    ".local/share/konsole/konsolestaterc".source = ./konsolestaterc;
   };
   programs.plasma.configFile.konsolerc = {
     "Desktop Entry".DefaultProfile.value = "konsole.profile";
+    # Why the hell is the toolbar configuration stored like this?
+    MainWindow.State = "AAAA/wAAAAD9AAAAAQAAAAAAAAAAAAAAAPwCAAAAAvsAAAAiAFEAdQBpAGMAawBDAG8AbQBtAGEAbgBkAHMARABvAGMAawAAAAAA/////wAAAXIA////+wAAABwAUwBTAEgATQBhAG4AYQBnAGUAcgBEAG8AYwBrAAAAAAD/////AAABEQD///8AAAdoAAAEAwAAAAQAAAAEAAAACAAAAAj8AAAAAQAAAAIAAAACAAAAFgBtAGEAaQBuAFQAbwBvAGwAQgBhAHIAAAAAAP////8AAAAAAAAAAAAAABwAcwBlAHMAcwBpAG8AbgBUAG8AbwBsAGIAYQByAAAAAAD/////AAAAAAAAAAA=";
     "Notification Messages".CloseAllTabs.value = true;
   };
 }
