383e1a408d1e080fe0d2e2d4aa88227379a9216f
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-08-26 20:45:48 +0200

    nixos/config/*: set editor to vim

--- nixos/config/_shared/global/default.nix
+++ nixos/config/_shared/global/default.nix
@@ -8,13 +8,12 @@
   imports = lib.allExceptThisDefault ./.;
 
   custom = {
-    impermanence.enable = true;
-
     hardware.external.yubikey.enable = true;
+    impermanence.enable = true;
   };
-
   environment = {
     pathsToLink = [ "/share/zsh" ];
+    stub-ld.enable = lib.mkForce false;
     systemPackages = with pkgs; [
       htop
       fontconfig
@@ -23,10 +22,18 @@
       steam-run
       vim
     ];
-
-    stub-ld.enable = lib.mkForce false;
+    variables.EDITOR = "vim";
   };
-
+  fonts.packages = with pkgs; [
+    corefonts
+    noto-fonts
+    noto-fonts-emoji
+    liberation_ttf
+  ];
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
   programs = {
     dconf.enable = true;
     firejail.enable = true;
@@ -34,46 +41,28 @@
     git.enable = true;
     zsh.enable = true;
   };
-
-  services = {
-    openssh = {
-      enable = true;
-
-      settings = {
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
-        PermitRootLogin = "no";
-      };
-    };
-
-    fwupd.enable = true;
-    mysql.package = pkgs.mariadb;
-    preload.enable = true;
-    ratbagd.enable = true;
-  };
-
-  users.users.root.hashedPassword = "!";
-
-  fonts.packages = with pkgs; [
-    corefonts
-    noto-fonts
-    noto-fonts-emoji
-    liberation_ttf
-  ];
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
   security.sudo = {
     enable = true;
     extraConfig = ''
       Defaults		timestamp_timeout=1
     '';
   };
-
   systemd.extraConfig = ''
     DefaultTimeoutStopSec=3s
   '';
+  services = {
+    fwupd.enable = true;
+    mysql.package = pkgs.mariadb;
+    openssh = {
+      enable = true;
+      settings = {
+        KbdInteractiveAuthentication = false;
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+      };
+    };
+    preload.enable = true;
+    ratbagd.enable = true;
+  };
+  users.users.root.hashedPassword = "!";
 }
