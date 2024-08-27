4d14e494f46b0846f5af30c3ae3b94f3a4fbecf2
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-08-27 16:32:02 +0200

    nixos/config/*/nix: enable distributed builds

--- nixos/config/_shared/global/nix.nix
+++ nixos/config/_shared/global/nix.nix
@@ -2,11 +2,34 @@
   inputs,
   lib,
   outputs,
+  pkgs,
   ...
 }:
 
 {
   nix = {
+    buildMachines =
+      let
+        ssh = {
+          sshKey = "/nix/nixbuild_id_ed25519";
+          sshUser = "nixbuild";
+        };
+      in
+      lib.map (machine: machine // ssh) [
+        {
+          hostName = "andromeda";
+          maxJobs = 4;
+          speedFactor = 1;
+          system = "x86_64-linux";
+        }
+        {
+          hostName = "thinkpad";
+          maxJobs = 8;
+          speedFactor = 2;
+          system = "x86_64-linux";
+        }
+      ];
+    distributedBuilds = true;
     nixPath = [ "nixpkgs=${inputs.nixpkgs.outPath}" ];
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
 
@@ -19,6 +42,7 @@
     settings = {
       auto-optimise-store = true;
       experimental-features = "nix-command flakes";
+      trusted-users = [ "nixbuild" ];
     };
   };
 
@@ -30,5 +54,18 @@
       outputs.overlays.modifications
       outputs.overlays.stable
     ];
+  };
+  users = {
+    groups.nixbuild = {};
+    users.nixbuild = {
+      isSystemUser = true;
+      group = "nixbuild";
+      shell = pkgs.bash;
+
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJbqB/u25wyLkT2DW1ZW2cRsgjFcpWsjTm++tTsL4xn1 root@andromeda"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIsbYYc1NmPplLOmhbiY+E6PZrt5FNq6x75QGrvPSGHE root@thinkpad"
+      ];
+    };
   };
 }
