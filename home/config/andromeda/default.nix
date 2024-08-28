547501bae2c7b0c0f45166cdef4688e93765c2bc
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-08-28 18:09:02 +0200

    home/config/andromeda/firefox: enable wayland

--- home/config/andromeda/default.nix
+++ home/config/andromeda/default.nix
@@ -17,7 +17,6 @@
       inherit (pkgs) spek;
     };
     stateVersion = "23.05";
-    sessionVariables.MOZ_ENABLE_WAYLAND = 0; # TODO Remove when 130 is out
   };
   programs = {
     deemix.enable = true;
