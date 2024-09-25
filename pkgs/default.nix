3b7cc76e3229f26a58b0b2888de79a2252819927
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-09-25 10:42:17 +0200

    flake.lock: update

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,7 +6,7 @@
 
 rec {
   kara = pkgs.callPackage ./kara.nix { };
-  krohnkite = pkgs.callPackage ./krohnkite { };
+  krohnkite = pkgs.callPackage ./krohnkite.nix { };
   kwin-effects-geometry-change = pkgs.callPackage ./kwin-effects-geometry-change { };
   lightly-qt6 = pkgs.kdePackages.callPackage ./lightly-qt6.nix { };
   nix-ascii-art = pkgs.callPackage ./nix-ascii-art.nix { inherit rascii; };
