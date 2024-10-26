0c5f0a289bd66acc42a9d20cd0bbbef461f2983b
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-10-26 19:14:16 +0200

    switch.sh: add more messages

--- switch.sh
+++ switch.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$EUID" = 0 ]; then
-    echo "This script cannot be executed as root."
-    exit 1
-fi
-
-rm -f ~/.gtkrc-2.0
-
+echo -e "\033[1;34minfo:\033[0m copying files..."
 sudo rm -rf /tmp/nix-config
 sudo cp -r /nix/config /tmp/nix-config
 sudo chown root:root -R /tmp/nix-config
@@ -17,11 +11,11 @@ if test -f /etc/current-config; then
     EXIT_CODE=0
     diff -r --exclude=.git $(cat /etc/current-config) /tmp/nix-config > /dev/null 2>&1 || EXIT_CODE=$?
     if [ $EXIT_CODE = 0 ]; then
-        echo "No changes."
+        echo "\033[1;31merror:\033[0m no changes"
         exit 1
     fi
     diff -Naur --color=always --exclude=.git --exclude=result $(cat /etc/current-config) /tmp/nix-config | less -R
-    read -n 1 -p "Switch? (y/n)" confirm
+    read -n 1 -p "Switch? (y/n) " confirm
     if [ "$confirm" = "${confirm#[Yy]}" ]; then
         exit 1
     fi
@@ -29,11 +23,14 @@ else
     echo -e "\033[1;33mwarning:\033[0m /etc/current-config not found. Cannot show diff."
 fi
 
+echo -e "\n\033[1;34minfo:\033[0m pinging substituters..."
 substituters="https://cache.nixos.org"
 nixServePort=5000
 if [ "$(hostname)" = "andromeda" ] && ping -c 1 -W 1 thinkpad > /dev/null 2>&1; then
     substituters="http://thinkpad:$nixServePort $substituters"
+    echo -e "\033[1;34minfo:\033[0m found substituter: thinkpad"
 elif [ "$(hostname)" = "thinkpad" ] && ping -c 1 -W 1 andromeda > /dev/null 2>&1; then
     substituters="http://andromeda:$nixServePort $substituters"
+    echo -e "\033[1;34minfo:\033[0m found substituter: andromeda"
 fi
 sudo nixos-rebuild switch --option substituters "$substituters" --flake /tmp/nix-config#$HOST --log-format internal-json -v |& nom --json
