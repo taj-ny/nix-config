f8e2391c2450be3e3e0b7005414fd1167c0bff7d
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-10-26 19:14:53 +0200

    nixos/config/*: remove .gtkrc-2.0 on activation

--- nixos/config/_shared/global/user.nix
+++ nixos/config/_shared/global/user.nix
@@ -33,20 +33,20 @@ in
       homeConfig
     ] ++ (builtins.attrValues outputs.homeManagerModules);
   };
-
   users.users.${username} = {
     isNormalUser = true;
     hashedPasswordFile = "/nix/persist/passwords/${username}";
     extraGroups = [ "wheel" "adbusers" ];
     shell = pkgs.zsh;
-
     openssh.authorizedKeys.keys = [
       ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIrMW7NUE7i3/6AksQmq34RK1xL2krFA0ljv8AiPho9Z marcin@andromeda''
     ];
   };
-
   services.displayManager.autoLogin = {
     enable = true;
     user = username;
   };
+  system.userActivationScripts.removeConflictingFiles.text = ''
+    rm .gtkrc-2.0
+  '';
 }
