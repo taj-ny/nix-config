2345951475daf2970e906bcc44ccf69afbe1d8c7
taj-ny <79316397+taj-ny@users.noreply.github.com>
2025-01-06 13:09:01 +0100

    home/config/*/plasma/darkly: "fix" tabbar background color

--- home/config/_shared/programs/plasma/darkly.nix
+++ home/config/_shared/programs/plasma/darkly.nix
@@ -3,8 +3,10 @@
     darklyrc = {
       Common.CornerRadius.value = 8;
       Style = {
+        AdjustToDarkThemes.value = true;
         DolphinSidebarOpacity.value = 50;
         MenuOpacity.value = 80;
+        TabBGColor.value = "0,0,0,50";
         TabUseHighlightColor.value = true;
       };
     };
